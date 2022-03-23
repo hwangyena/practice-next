dd0ae781ba330f5715eea95f0d111e331d8008a2
hwangyena <skp.5003952@skplanet.com>
2022-03-23 15:30:58 +0900

    unstated-next 상태관리

---
+++ components/message.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Message = (props: Props) => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Message;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import GlobalState from '../store'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <GlobalState.Provider>
+  <Component {...pageProps} />
+  </GlobalState.Provider>
 }
 
 export default MyApp
